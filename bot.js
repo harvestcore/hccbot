c53b78cb4c995b2dc2e2b57717b288a7a598d392
harvestcore <aagomezies@gmail.com>
2018-11-09 13:03:46 +0100

    1st toot :v

---
+++ bot.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const Mastodon = require('mastodon-api');
+
+
+const M = new Mastodon({
+    client_key: process.env.CLIENT_KEY,
+    client_secret: process.env.CLIENT_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    timeout_ms: 60 * 1000,
+    api_url: 'https://botsin.space/api/v1/'
+});
+
+const params = {
+    status: 'howdy'
+};
+
+M.post('statuses', params, (error, data) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(data);
+    }
+});
